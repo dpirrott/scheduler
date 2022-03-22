6bc320aebf0ec9e9067d7d886857f6ad0e380190
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-03-22 11:34:47 -0700

    Create formatSpots function for dayListItem to change text depending on remaining spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,6 +7,17 @@ export default function DayListItem(props) {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0,
   });
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return 'no spots';
+    }
+    if (props.spots === 1) {
+      return '1 spot';
+    }
+    return `${props.spots} spots`;
+  };
+
   return (
     <li
       className={dayClass}
@@ -14,7 +25,7 @@ export default function DayListItem(props) {
         props.setDay(props.name);
       }}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()} remaining</h3>
     </li>
   );
 }
