50533de19c786b0d46ca4ebde93b1e83ee6ab2e7
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-03-23 23:50:40 -0700

    Add transition and back functions to useVisualMode, add history state as array of previous modes

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,5 +2,27 @@ import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  return { mode };
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    const addHistory = history;
+    if (replace) {
+      addHistory[history.length - 1] = newMode;
+    } else {
+      addHistory.push(newMode);
+    }
+    setHistory(addHistory);
+    setMode(newMode);
+  };
+
+  const back = () => {
+    if (history.length > 1) {
+      const rmHistory = history;
+      rmHistory.pop();
+      setHistory(rmHistory);
+      setMode(rmHistory[rmHistory.length - 1]);
+    }
+  };
+
+  return { mode, transition, back };
 }
